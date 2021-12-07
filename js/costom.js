b9ca2a76b4ce717976766dc8cf3b061b0f4d02aa
imranahnaf <mdemranhossen147@gamil.com>
2021-12-07 18:23:43 +0600

    jackpot complite

--- js/costom.js
+++ js/costom.js
@@ -2,9 +2,37 @@ $(function () {
     new WOW().init();
     // ****** pricing animate css js ****** //
     
-
+  
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+    let birthday = "Jan 01, 2022 00:00:00",
+        countDown = new Date(birthday).getTime(),
+        x = setInterval(function () {
+            let now = new Date().getTime(),
+                distance = countDown - now;
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            //do something later when date is reached
+            if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown"),
+                    content = document.getElementById("content");
+                headline.innerText = "It's my birthday!";
+                countdown.style.display = "none";
+                content.style.display = "block";
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
 });
+//countdown timer js 
 
+
+// ****** routine section js ***** //
 $('.para1').hide();
 $('.para2').hide();
 $('.para3').hide();
@@ -26,7 +54,12 @@ $(document).ready(function () {
     $(".item4").click(function () {
 		$('.para4').toggle();
     });
-    
 });
+// ****** routine section js ***** //
 
-        
+
+
+
+
+
+
